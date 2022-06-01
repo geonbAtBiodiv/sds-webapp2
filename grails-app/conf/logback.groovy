6fb512654ed0d5cede98a266a9f46e40ea750a50
pal155 <Doug.Palmer@csiro.au>
2022-06-01 11:57:02 +1000

    Logging to files

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,36 +1,98 @@
-import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
 import java.nio.charset.Charset
 
+def loggingDir = (System.getProperty('catalina.base') ? System.getProperty('catalina.base') + '/logs' : './logs')
+def appName = 'sds'
+final TOMCAT_LOG = 'TOMCAT_LOG'
+final FULL_STACKTRACE = 'FULL_STACKTRACE'
+final STDOUT = 'STDOUT'
+
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-appender('STDOUT', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+switch (Environment.current) {
+    case Environment.PRODUCTION:
+        appender(TOMCAT_LOG, RollingFileAppender) {
+            file = "${loggingDir}/${appName}.log"
+            encoder(PatternLayoutEncoder) {
+                pattern =
+                        '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                                '%5p ' + // Log level
+                                '--- [%15.15t] ' + // Thread
+                                '%-40.40logger{39} : ' + // Logger
+                                '%m%n%wex' // Message
+            }
+            rollingPolicy(FixedWindowRollingPolicy) {
+                fileNamePattern = "${loggingDir}/${appName}.%i.log.gz"
+                minIndex = 1
+                maxIndex = 4
+            }
+            triggeringPolicy(SizeBasedTriggeringPolicy) {
+                maxFileSize = FileSize.valueOf('10MB')
+            }
+        }
+        root(WARN, [TOMCAT_LOG])
+        logger('au.org.ala', INFO)
+        logger('grails.app.controllers.au.org.ala', INFO)
+        logger('grails.app.services.au.org.ala', INFO)
+        break
+    case Environment.TEST:
+        appender(TOMCAT_LOG, RollingFileAppender) {
+            file = "${loggingDir}/${appName}.log"
+            encoder(PatternLayoutEncoder) {
+                pattern =
+                        '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                                '%5p ' + // Log level
+                                '--- [%15.15t] ' + // Thread
+                                '%-40.40logger{39} : ' + // Logger
+                                '%m%n%wex' // Message
+            }
+            rollingPolicy(FixedWindowRollingPolicy) {
+                fileNamePattern = "${loggingDir}/${appName}.%i.log.gz"
+                minIndex = 1
+                maxIndex = 4
+            }
+            triggeringPolicy(SizeBasedTriggeringPolicy) {
+                maxFileSize = FileSize.valueOf('1MB')
+            }
+        }
+        root(INFO, [TOMCAT_LOG])
+        break
+    case Environment.DEVELOPMENT:
+        appender(STDOUT, ConsoleAppender) {
+            encoder(PatternLayoutEncoder) {
+                charset = Charset.forName('UTF-8')
+                pattern =
+                        '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                                '%clr(%5p) ' + // Log level
+                                '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                                '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                                '%m%n%wex' // Message
+            }
+        }
+        appender(FULL_STACKTRACE, FileAppender) {
+            file = "${loggingDir}/stacktrace.log"
+            append = true
+            encoder(PatternLayoutEncoder) {
+                pattern = '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
+                        "%level %logger - %msg%n"
+            }
+        }
+        root(INFO, [FULL_STACKTRACE, STDOUT])
+        break
+    default:
+        appender(TOMCAT_LOG, ConsoleAppender) {
+            encoder(PatternLayoutEncoder) {
+                pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
                         '%clr(%5p) ' + // Log level
                         '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
                         '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
                         '%m%n%wex' // Message
-    }
-}
-
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
+            }
         }
-    }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+        root(INFO, [TOMCAT_LOG])
+        break
 }
-root(ERROR, ['STDOUT'])
